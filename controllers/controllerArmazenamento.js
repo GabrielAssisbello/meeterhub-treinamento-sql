fcdfd6aaa7523b7f14175b7f1f415550bbd8e5b5
Gabriel Assis <gassis574@gmail.com>
2023-11-01 16:27:33 -0300

    controller/model armazenamento adicionado

---
+++ controllers/controllerArmazenamento.js
@@ -0,0 +1,68 @@
+const ModeloArmazenamento = require('../models/modelArmazenamento.js');
+
+class ControladorArmazenamento{
+    async criarArmazenamento( req, res ){
+        const Autor = req.body;
+        try{
+            const idArmazenamento= await ModeloArmazenamento.criarArmazenamento(Armazenamento);
+            res.status(201).json({ id: idArmazenamento});
+        } catch( erro ){
+            res.status(500).json({ erro: 'Erro ao criar um armazenamento'});
+        }
+    }
+
+    async obterTodosArmazenamentos( req, res ){
+        try{
+            const armazenamentos = await ModeloAutor.obterTodosArmazenamentos();
+            res.status(200).json(autores);
+        } catch( erro ){
+            res.status(500).json({ erro: 'Erro ao buscar todos os armazenamentos'});
+        }
+    }
+
+    async obterArmazenamentoPorId( req, res ){
+        const id = req.params.id;
+        try{
+            const armazenamento = await ModeloArmazenamento.obterArmazenamentoPorId(id);
+            if( armazenamento ){
+                res.status(200).json(armazenamento);
+            } else {
+                res.status(404).json({erro: 'armazenamento não encontrado'})
+            }
+        } catch( erro ){
+            res.status(500).json({erro: 'Erro ao buscar o armazenamento'});
+        }
+    }
+
+    async atualizarArmazenamento( req, res ){
+        const id = req.params.id;
+        const autor = req.body; 
+        try{
+            const resultado = await ModeloAutor.atualizarArmanezamento(id_livro, id_deposito);
+            if( resultado ){
+                res.status(200).json({msg: 'Armazenamento atualizado com sucesso'});
+            } else {
+                res.status(404).json({erro: 'Armazenamento não encontrado'})
+            }
+        } catch( erro ){
+            res.status(500).json({ erro: 'Erro ao atualizar Armazenamento'});
+        }
+    }
+    
+    async excluirArmazenamento( req, res ){
+        const id = req.params.id;
+        try{
+            const resultado = await ModeloAutor.excluirArmazenamento(id);
+            if( resultado ){
+                res.status(200).json({msg: 'Armazenamento excluido com sucesso'});
+            } else {
+                res.status(404).json({erro: 'Armazenamento não encontrado'})
+            }
+        } catch( erro ){
+            res.status(500).json({ erro: 'Erro ao excluir Armazenamento'});
+        }
+    }
+
+}
+
+module.exports = new ControladorArmazenamento();
---
+++ models/modelArmazenamento.js
@@ -0,0 +1,78 @@
+const mysql = require('mysql2/promise');
+
+class ModeloArmazenamento{
+    constructor(){
+        this.pool = mysql.createPool({
+            host:'127.0.0.1',
+            user: 'root',
+            password:'gabriel',
+            database:'db_cliente',
+        });
+    }
+
+    async criarArmazenamento(autor) {
+        const connection = await this.pool.getConnection();
+        try{
+            const [resultado] = await connection.query(
+                'insert into tb_armazenamenti (id.livro, id_deposito) values(?, ?)',
+                [armazenamento.id.livro, armazenamento.id_deposito]
+            );
+            return resultado.insertId;
+        } finally {
+            connection.release();
+        }
+    }
+
+    async obterTodosArmazenamento() {
+        const connection = await this.pool.getConnection();
+        try{
+            const [registros] = await connection.query(
+                'select * from tb_armazenamento'
+            );
+            return registros;
+        } finally {
+            connection.release();
+        }
+    }
+
+    async obterArmazenamentoPorId(id) {
+        const connection = await this.pool.getConnection();
+        try{
+            const [registros] = await connection.query(
+                'select * from tb_armazenamento where id = ?',
+                [id]
+            );
+            return registros[0];
+        } finally {
+            connection.release();
+        }
+    }
+
+    async atualizarArmazenamento(id, cliente) {
+        const connection = await this.pool.getConnection();
+        try{
+            await connection.query(
+                'update tb_autor set id.livro = ? , id_deposito = ? where id = ?',
+                [armazenamento.id.livro, armazenamento.id_deposito]
+            );
+            return true;
+        } finally {
+            connection.release();
+        }
+    }
+
+    async excluirArmazenamento(id) {
+        const connection = await this.pool.getConnection();
+        try{
+            await connection.query(
+                'delete from tb_armazenamento where id = ?',
+                [id]
+            );
+            return true;
+        } finally {
+            connection.release();
+        }
+    }
+}
+
+module.exports = new ModeloArmazenamento();
